2dfac0548c8a6802a8edd2fed5d9cdd2ce9b921a
TXmas <nushaxmas@gmail.com>
2022-09-17 17:53:28 +0300

    Lesson2 HW

---
+++ Lesson2_Seminar1_HW/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string answer = "";
+int n = 2;
+
+while (n <= number)
+{
+    if (n > 2) answer += ", ";
+    answer += n;
+    n += 2;
+}
+
+Console.WriteLine(answer);
