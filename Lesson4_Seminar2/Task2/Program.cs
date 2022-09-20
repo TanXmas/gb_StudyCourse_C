32e8e0cb0d67cc07b1c6828b613d162b1e3b3ce8
TXmas <nushaxmas@gmail.com>
2022-09-20 21:56:45 +0300

    Lesson4 update

--- Lesson4_Seminar2/Task2/Program.cs
+++ Lesson4_Seminar2/Task2/Program.cs
@@ -3,18 +3,11 @@
 // 782 -> 72
 // 918 -> 98
 
-int getRandomNumberFromRange(int minb, int maxb)
-{
-    int res = new int();
-    res = new Random().Next(minb, maxb+1);
-    return res;
-}
-
 int getNewNum(int num)
 {
     return num / 100 * 10 + num % 10;
 }
 
-int randNum = getRandomNumberFromRange(100,999);
+int randNum = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число {randNum}.");
 Console.WriteLine($"Новое число {getNewNum(randNum)}.");
