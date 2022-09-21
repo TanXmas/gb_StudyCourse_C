c1f13d1e87db9b0299e43645ba04afe7317a5fa8
TXmas <nushaxmas@gmail.com>
2022-09-21 20:31:39 +0300

    Lesson4 HW update

---
+++ Lesson4_Seminar2_HW/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+int getNumberDayOfWeek(int numberDayOfWeek)
+{
+    while (numberDayOfWeek < 1 | numberDayOfWeek > 7)
+    {
+        Console.Write("Введите номер дня недели: ");
+        numberDayOfWeek = Convert.ToInt32(Console.ReadLine());    
+    }
+    return numberDayOfWeek;
+}
+
+int numberDayOfWeek = 0;
+numberDayOfWeek = getNumberDayOfWeek(numberDayOfWeek);
+
+if (numberDayOfWeek < 6)
+{
+    Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine("Да");
+}
