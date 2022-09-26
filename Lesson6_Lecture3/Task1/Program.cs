4fe4c6a37484f22360990c4f48e024b0ec2ab2c2
TXmas <nushaxmas@gmail.com>
2022-09-26 20:22:00 +0300

    Lesson6

---
+++ Lesson6_Lecture3/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+/*
+Дан текст. В тексте нужно все пробелы заменить черточками,
+маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+маленькими “с”.
+*/
+
+string replaceChar(string text, char oldChar, char newChar)
+{
+    string res = String.Empty;
+    int len = text.Length;
+    for (int i = 0; i < len; i ++)
+    {
+        if (text[i] == oldChar)
+            res += $"{newChar}";
+        else
+            res += $"{text[i]}";
+    }
+    return res;
+}
+
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, " +
+              "ежели бы вас послали вместо нашего милого " +
+              "Винценгероде, вы бы взяли приступом согласие " +
+              "прусского короля. Вы так красноречивы. Вы " +
+              "дадите мне чаю?";
+
+string rep1 = replaceChar(text, ' ', '_');
+Console.WriteLine(rep1);
+
+string rep2 = replaceChar(rep1, 'к', 'К');
+Console.WriteLine(rep2);
+
+string rep3 = replaceChar(rep2, 'В', 'в');
+Console.WriteLine(rep3);
---
+++ Lesson6_Lecture3/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// сортировка массива
+
+void printArray(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void sortArray(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int minPos = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPos]) minPos = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+sortArray(arr);
+printArray(arr);
