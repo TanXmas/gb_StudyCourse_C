2be864d2ec623527398755e2a4ad385e06edcfb8
TXmas <nushaxmas@gmail.com>
2022-11-10 17:13:49 +0300

    FinalWork code

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿void printArray (string[] array)
+{  
+    int len = array.Length;
+    int prev = -1;
+    Console.Write("[");
+    for (int i = 0; i < len; i ++)
+    {
+        if (!string.IsNullOrEmpty(array[i]))
+            {
+                if (prev > -1) Console.Write(", ");
+                Console.Write(array[i]);
+                prev = i;
+            }
+    }
+    Console.WriteLine("]");
+}
+
+
+void fillArrayOut (string[] arrayIn, string[] arrayOut)
+{
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        if (arrayIn[i].Length <= 3)
+        {
+            arrayOut[i] = arrayIn[i];
+        }
+    }
+}
+
+
+string[] arrayInput = new string[]{"Hello", "world", "!", ":-)", "1234", "-2", "computer science", "RU", "Russia"};
+string[] arrayOutput = new string[arrayInput.Length];
+fillArrayOut(arrayInput, arrayOutput);
+printArray(arrayOutput);
